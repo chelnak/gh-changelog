35fc92015af5850db68ccf90b2c870d4206601cb
Craig Gumbley <craiggumbley@gmail.com>
2022-05-08 21:23:07 +0100

    Add a test for configuration setup
    This commit adds a basic test to ensure that the configuration struct
    is being initialized correctly.

--- internal/pkg/configuration/configuration_test.go
+++ internal/pkg/configuration/configuration_test.go
@@ -1,1 +1,34 @@
 package configuration_test
+
+import (
+	"testing"
+
+	"github.com/chelnak/gh-changelog/internal/pkg/configuration"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInitConfigSetsCorrectValues(t *testing.T) {
+	err := configuration.InitConfig()
+	assert.NoError(t, err)
+
+	config := configuration.Config
+
+	assert.Equal(t, "CHANGELOG.md", config.FileName)
+
+	assert.Equal(t, []string{"maintenance"}, config.ExcludedLabels)
+	assert.Equal(t, 1, len(config.ExcludedLabels))
+
+	assert.True(t, containsKey(config.Sections, "Changed"))
+	assert.True(t, containsKey(config.Sections, "Added"))
+	assert.True(t, containsKey(config.Sections, "Fixed"))
+
+	assert.Equal(t, 3, len(config.Sections))
+	assert.Equal(t, false, config.SkipEntriesWithoutLabel)
+	assert.Equal(t, true, config.ShowUnreleased)
+	assert.Equal(t, true, config.CheckForUpdates)
+}
+
+func containsKey(m map[string][]string, key string) bool {
+	_, ok := m[key]
+	return ok
+}
