13ec54d6e0afc84dbf6fd41aa5b90f624db932e7
Craig Gumbley <craiggumbley@gmail.com>
2022-10-07 20:08:05 +0100

    Add new version flags
    Prior to this commit it was not possible to scope the changelog to a
    specific tag.
    
    This commit adds two new flags that will allow a user to either specify
    a tag to start from or build the changelog starting from the latest tag.
    
    `--from-version`
    
    This flag determines the version from which to start the changelog.
    If the value passed does not exist as a tag, the changelog will
    be built from the first tag.
    
    `--from-last-version`
    
    This flag will build the changelog starting from the last tag.
    Using this flag will result in a changelog with one entry.

--- cmd/new.go
+++ cmd/new.go
@@ -13,6 +13,8 @@ import (
 )
 
 var nextVersion string
+var fromVersion string
+var fromLastVersion bool
 
 // newCmd is the entry point for creating a new changelog
 var newCmd = &cobra.Command{
@@ -23,6 +25,8 @@ var newCmd = &cobra.Command{
 		builder := changelog.NewChangelogBuilder()
 		builder = builder.WithSpinner(true)
 		builder = builder.WithNextVersion(nextVersion)
+		builder = builder.WithFromVersion(fromVersion)
+		builder = builder.WithFromLastVersion(fromLastVersion)
 
 		changelog, err := builder.Build()
 		if err != nil {
@@ -40,4 +44,21 @@ var newCmd = &cobra.Command{
 
 func init() {
 	newCmd.Flags().StringVar(&nextVersion, "next-version", "", "The next version to be released. The value passed does not have to be an existing tag.")
+
+	newCmd.Flags().StringVar(
+		&fromVersion,
+		"from-version",
+		"",
+		"The version from which to start the changelog. If the value passed does not exist as a tag,\nthe changelog will be built from the first tag.",
+	)
+
+	newCmd.Flags().BoolVar(
+		&fromLastVersion,
+		"from-last-version",
+		false,
+		"Build the changelog starting from the last tag. Using this flag will result in a changelog with one entry.\nIt can be useful for generating a changelog to be used in release notes.",
+	)
+
+	newCmd.MarkFlagsMutuallyExclusive("from-version", "from-last-version")
+	newCmd.Flags().SortFlags = false
 }
