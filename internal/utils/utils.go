097ae95ff36aadbca0ee323a1714dc3fcc5bb327
Craig Gumbley <craiggumbley@gmail.com>
2023-02-09 21:42:04 +0000

    Extract repo context in to util method

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 
 	"github.com/Masterminds/semver/v3"
+	"github.com/cli/go-gh"
 	"github.com/fatih/color"
 )
 
@@ -94,4 +95,27 @@ func parseLocalVersion(version string) string {
 	}
 
 	return slice[2]
+}
+
+// RepoContext is a struct that contains the current repository owner and name.
+type RepoContext struct {
+	Owner string
+	Name  string
+}
+
+// GetRepoContext returns a new RepoContext struct with the current repository owner and name.
+func GetRepoContext() (RepoContext, error) {
+	currentRepository, err := gh.CurrentRepository()
+	if err != nil {
+		if strings.Contains(err.Error(), "not a git repository (or any of the parent directories)") {
+			return RepoContext{}, fmt.Errorf("the current directory is not a git repository")
+		}
+
+		return RepoContext{}, err
+	}
+
+	return RepoContext{
+		Owner: currentRepository.Owner(),
+		Name:  currentRepository.Name(),
+	}, nil
 }
